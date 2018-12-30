458909cd679b8efd71ca0f364e8ab8b0a128bef2
Eoussama <oussama_gtacj@live.fr>
2018-12-30 15:13:39 +0100

    Updated the files.

---
+++ docs/assets/js/main.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @name:       typewriterjs
+ * @version:    4.0.0
+ * @author:     EOussama
+ * @license     MIT
+ * @source:     https://github.com/EOussama/typewriterjs
+ *
+ * A typewriter for the web.
+ * 
+ */
+	
+
+console.log('Hello, world!');
