f4608e8ae04ed4d4a1f268f79edbd46c8ce48d64
Eoussama <oussama_gtacj@live.fr>
2019-01-05 18:11:29 +0100

    README.md updated along with the docs.

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -9,9 +9,37 @@
  * A typewriter for the web.
  * 
  */
+
+
+window.addEventListener('load', function () {
+
+	var
+		header = document.getElementsByTagName('header')[0],
+		logo = document.querySelector('header img'),
+		main = document.getElementsByTagName('main')[0],
+		title = document.getElementById('title'),
+		titleTw = new Typewriter({
+			target: title,
+			speed: 120
+		});
+		
+		// Typing the title.
+		titleTw.type({
+			script: "TypewriterJS",
+			endCallback: function () {
+				
+				// Changing the header's styling and revealing the content.
+				header.style.height = '10vh';
+				header.style.padding = '150px 10vh';
+				logo.style.width = '150px';
+				logo.style.marginTop = '-50px';
+				document.body.style.overflow = 'auto';
+				main.style.display = 'block';
+			}
+		});
 	
-
-window.addEventListener('load', () => {
-
-	console.log('Hello, world!');
+		// Highlighting the code snippets.
+		document.querySelectorAll('pre > code').forEach(function(block) {
+			hljs.highlightBlock(block);
+		});
 });
