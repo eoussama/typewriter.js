b54261d0a0473ae79f2f38a684eb8ac74c3a8b02
Oussama Essamadi <Oussama_gtacj@live.fr>
2021-11-12 12:16:02 +0100

    ⬅ Queue

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -16,8 +16,37 @@ export class Queue<T> {
    */
   get length() { return this.items.length }
 
-  enqueue() { }
-  dequeue() { }
-  isEmpty() { }
-  peek() { }
+  /**
+   * @description
+   * Queues the target item in
+   *
+   * @param item The target iteme
+   */
+  enqueue(item: T): void {
+    this.items.push(item);
+  }
+
+  /**
+   * @description
+   * Dequeues the first item
+   */
+  dequeue(): T { 
+    return this.items.shift();
+  }
+
+  /**
+   * @description
+   * Checks if the list is empty
+   */
+  isEmpty(): boolean { 
+    return this.items.length === 0;
+  }
+
+  /**
+   * @description
+   * Returns the first item n the queue
+   */
+  peek(): T { 
+    return this.items[0];
+  }
 }
